df41d3ff125fc33f4ee9605f5c53191e360846fc
ajnurademi <ajnur.ademi@hotmail.com>
2025-02-27 11:54:47 +0100

    initialized project

---
+++ app/(root)/(tabs)/rulebook.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Rulebook = () => {
+  return (
+    <View>
+      <Text>Rulebook</Text>
+    </View>
+  )
+}
+
+export default Rulebook
